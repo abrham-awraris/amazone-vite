6841fd0d5dbf048b321688ec2bb8b309f6cc891d
abrham-awraris <abriloodesta@gmail.com>
2024-12-14 04:05:24 +0300

    Fix build configuration and add dist folder

--- amazone-clone/vite.config.js
+++ amazone-clone/vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Set the output directory to 'dist'
+    rollupOptions: {
+      input: 'index.html',  // Use correct path to the index.html
+    },
+  }
+  
 })
 
